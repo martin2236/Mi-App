6f718ebc6dcfcaaccafb6a3976066ed4bfffae60
Martin <martoxxx100@gmail.com>
2021-06-18 19:52:52 -0300

    cree un 'manual' en el index

--- index.ts
+++ index.ts
@@ -8,22 +8,27 @@ return resultado;
 }
 function objetos (control ,params){
  
-    
+    //regresa los ids
  const id = params.id.map((item: any)=>{
        return {id:item}
    })
+   //regresa las cantidades
   const cantidad = params.cantidad.map((item:any)=>{
       return {cantidad:item}
   }) 
-
+  //une las cantidades
 var listaUnida = lodash.merge(id,cantidad)
+
+var resultado = []
+
 var pedidos = lodash.forEach(listaUnida, (item)=>{
  return control.get(item).then((res)=>{
-  console.log(res)
+ return(res)
 })
 })
 
-return pedidos
+
+return resultado
 
 }
 
@@ -43,3 +48,5 @@ main()
 //Tercero indicar la cantidad deseada del plato ej: --cantidad=3
 //Al estar terminada la parte logica deberia funcionar con botones predeterminados en una pagina web para no
 //dar margen a escribir opciones incorrectas
+
+//atajo ej(copiar y pegar en la consola) mesa1 --id=1 --cantidad=2 --id=2 --cantidad=2
